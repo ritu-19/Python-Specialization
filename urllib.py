d5be2379f8952a7f93b8e17ade75fec27ae50020
Ritu Yadav <38189524+ritu-19@users.noreply.github.com>
2018-07-31 22:29:52 +0530

    Access data from web code

---
+++ urllib.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 19 12:43:42 2018
+
+@author: ritu
+"""
+
+# To run this, you can install BeautifulSoup
+# https://pypi.python.org/pypi/beautifulsoup4
+
+# Or download the file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+
+# html.parser is the HTML parser included in the standard Python 3 library.
+# information on other HTML parsers is here:
+# http://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-a-parser
+soup = BeautifulSoup(html, "html.parser")
+
+# Retrieve all of the anchor tags
+tags = soup('span')
+sum_numbers = 0
+for tag in tags:
+    sum_numbers = sum_numbers + int(tag.contents[0])
+    
+print('Sum: ' , sum_numbers) 
